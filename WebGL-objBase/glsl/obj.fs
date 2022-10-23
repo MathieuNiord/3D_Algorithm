d992cf81cf595d70eba296d01537d0978a4cdb8d
Mathieu <mathieu.niord@etu.univ-poitiers.fr>
2022-10-23 15:25:05 +0200

    Fixes

--- WebGL-objBase/glsl/obj.fs
+++ WebGL-objBase/glsl/obj.fs
@@ -1,4 +1,3 @@
-
 precision mediump float;
 
 varying vec4 pos3D;
@@ -16,15 +15,10 @@ void main(void)
         vec3 I = normalize(-pos3D.xyz);
         vec3 R = reflect(I,normals);
         vec3 direction = mat3(uInversedRotationMatrix) * R;
-		vec3 col = vec3(0.8,0.4,0.4) * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
-        gl_FragColor = textureCube(uSampler, -direction.xzy) * vec4(col,1.0);
+        gl_FragColor = textureCube(uSampler, -direction.xzy);
 	}
 	else {
 		vec3 col = vec3(0.8,0.4,0.4) * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
 		gl_FragColor = vec4(col,1.0);
 	}
 }
-
-
-
-
