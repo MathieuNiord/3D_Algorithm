d660e884a3cce6735714cf1fb2722268e328b0da
Mathieu Niord <mathieu.niord@etu.univ-poitiers.fr>
2022-11-10 16:53:41 +0100

    Added Reflection + Transmission

--- WebGL-objBase/glsl/geometry.fs
+++ WebGL-objBase/glsl/geometry.fs
@@ -7,36 +7,42 @@ varying vec3 vNormal;
 uniform samplerCube uSampler;
 uniform mat4 uRotationMatrix;
 uniform bool uIsMirroring;
-uniform bool uIsReflecting;
+uniform bool uIsTransmitting;
 uniform float uFresnelIndice;
     
-void main() {
-
+void main()
+{
     vec3 normals = normalize(vNormal);
 	vec3 I = normalize(-vPosition.xyz);
 	vec3 reflectDirection = mat3(uRotationMatrix) * reflect(-I,normals);
 	vec3 transmissionDirection = mat3(uRotationMatrix) * refract(-I, normals, 1.0 / uFresnelIndice);
 
-    if (uIsMirroring) {
-        gl_FragColor = textureCube(uSampler, -reflectDirection.xzy);
-    }
+	if (uIsMirroring && uIsTransmitting) {
 
-    else if (uIsReflecting) {
 		float c = dot(I, normals);
         float g = sqrt((uFresnelIndice * uFresnelIndice) + (c * c) - 1.0);
         float left = 0.5 * (((g - c) * (g - c)) / ((g + c) * (g + c)));
         float right = 1.0 + (((c * (g + c) - 1.0) * (c * (g + c) - 1.0)) / ((c * (g - c) + 1.0) * (c * (g - c) + 1.0)));
+		
 		float reflection = left * right;
 		float transmission = 1.0 - reflection;
 		
 		vec4 colorReflection = vec4(textureCube(uSampler, reflectDirection.xzy).xyz * reflection, 1.0);
 		vec4 colorTransmission = vec4(textureCube(uSampler, transmissionDirection.xzy).xyz * transmission, 1.0);
 
-		gl_FragColor = colorTransmission + colorReflection;
+		gl_FragColor = colorTransmission + colorReflection;	
 	}
 
-    else {
-        vec3 col = vColor * dot(vNormal,normalize(vec3(-vPosition))); // Lambert rendering, eye light source
-        gl_FragColor = vec4(col,1.0);
-    }
+	else if (uIsMirroring) {
+        gl_FragColor = textureCube(uSampler, reflectDirection.xzy);
+	}
+
+	else if (uIsTransmitting) {
+		gl_FragColor = textureCube(uSampler, transmissionDirection.xzy);
+	}
+
+	else {
+		vec3 col = vec3(vColor.xyz) * dot(vNormal,normalize(vec3(-vPosition))); // Lambert rendering, eye light source
+		gl_FragColor = vec4(col,1.0);
+	}
 }
